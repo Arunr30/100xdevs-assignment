99af7c2e7c23f760b32578672b96bc3c5dbac5f8
Arunvasuacchu <arunvasur@gmail.com>
2024-09-02 11:48:52 +0900

    todo practice and week 3.1 completed

---
+++ week-3/dom.js
@@ -0,0 +1,21 @@
+let ctr = 1
+function deleteTodo(index) {
+    const deleteEl = document.getElementById(index)
+    deleteEl.parentNode.removeChild(deleteEl)
+}
+
+function addTodo() {
+    const inputEl = document.querySelector('input')
+    const value = inputEl.value
+
+    const divEl = document.createElement('div')
+    divEl.setAttribute('id', ctr)
+    divEl.innerHTML =
+        '<div>' +
+        value +
+        "</div><button onclick ='deleteTodo(" +
+        ctr +
+        ")'>delete</button>"
+    document.querySelector('body').appendChild(divEl)
+    ctr = ctr + 1
+}

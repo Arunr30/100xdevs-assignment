abcb49e552915dfa6f01c7f3c620dc2438730815
Arunvasuacchu <arunvasur@gmail.com>
2024-09-03 21:32:17 +0900

    side bar completed

---
+++ e-commerce/script.js
@@ -0,0 +1,9 @@
+let sidenav = document.querySelector('.side-navbar')
+
+function showNavBar() {
+    sidenav.style.display = 'block'
+}
+
+function closeNavBar() {
+    sidenav.style.display = 'none'
+}

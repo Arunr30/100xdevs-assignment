d8263a27f8063a8106998f59fb0fccc31d1f79b3
Arunvasuacchu <arunvasur@gmail.com>
2024-09-03 23:03:33 +0900

    just wasted so much time

--- e-commerce/script.js
+++ e-commerce/script.js
@@ -1,9 +1,9 @@
 let sidenav = document.querySelector('.side-navbar')
 
 function showNavBar() {
-    sidenav.style.display = 'block'
+    sidenav.style.left = '0%'
 }
 
 function closeNavBar() {
-    sidenav.style.display = 'none'
+    sidenav.style.left = '-60%'
 }

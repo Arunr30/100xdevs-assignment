cc6a2e1c4350f51372a812559f80c3e2fe1075c5
Arunvasuacchu <arunvasur@gmail.com>
2024-09-11 11:39:31 +0530

    express real world

---
+++ week-4.1/exp.js
@@ -0,0 +1,13 @@
+let users = [
+    {
+        name: 'John',
+        Kidneys: [
+            {
+                healthy: false,
+            },
+            {
+                healthy: true,
+            },
+        ],
+    },
+]

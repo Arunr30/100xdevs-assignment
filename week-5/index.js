d86a27f593cf191ad131e9065e5a8bfe31ae5baa
Arunvasuacchu <arunvasur@gmail.com>
2024-09-15 11:05:07 +0530

    week-5 started

--- week-5/index.js
+++ week-5/index.js
@@ -1,4 +1,23 @@
 const express = require("express");
 const app = express();
 
+function allow(req, res, next) {
+  const age = req.query.age;
+  if (age > 18) {
+    next();
+  } else {
+    res.json({
+      msg: "you are fucked!",
+    });
+  }
+}
+
+app.use(allow);
+
+app.get("/", function (req, res) {
+  res.json({
+    msg: "all set",
+  });
+});
+
 app.listen(3000);

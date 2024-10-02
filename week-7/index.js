a531072bacac6d37201c88c3f58fc4046b6209f8
Arunvasuacchu <arunvasur@gmail.com>
2024-10-02 18:32:42 +0530

    implemented zod input validation

--- week-7/index.js
+++ week-7/index.js
@@ -2,8 +2,9 @@ const bcrypt = require('bcrypt')
 const express = require('express')
 const { UserModel, TodoModel } = require('./db')
 const jwt = require('jsonwebtoken')
-const { mongoose } = require('mongoose')
 const JWT_SECRET = 'darkknight'
+const { mongoose } = require('mongoose')
+const { z } = require('zod')
 mongoose.connect(
     'mongodb+srv://arunvasur:WGgxms36fZSYRbzt@cluster0.cqc6n.mongodb.net/todo-app-new-vscode'
 )
@@ -12,28 +13,41 @@ const app = express()
 app.use(express.json())
 
 app.post('/signup', async (req, res) => {
-    try {
-        const email = req.body.email
-        const password = req.body.password
-        const name = req.body.name
+    const requiredBody = z.object({
+        email: z.string().min(3).max(40).email(),
+        password: z.string().min(3).max(100),
+        name: z.string().min(3).max(100),
+    })
 
-        const hashedPassword = await bcrypt.hash(password, 5)
-        console.log(hashedPassword)
-
-        await UserModel.create({
-            email: email,
-            password: hashedPassword,
-            name: name,
-        })
+    const parsedDatawithSuccess = requiredBody.safeParse(req.body)
+    if (!parsedDatawithSuccess.success) {
         res.json({
-            msg: 'You are signed up!',
-        })
-    } catch (e) {
-        res.status(403).send({
-            msg: 'User already exists!',
+            msg: 'not correct details',
+            error: parsedDatawithSuccess.error,
         })
         return
     }
+
+    const email = req.body.email
+    const password = req.body.password
+    const name = req.body.name
+
+    const hashedPassword = await bcrypt.hash(password, 5)
+    console.log(hashedPassword)
+
+    await UserModel.create({
+        email: email,
+        password: hashedPassword,
+        name: name,
+    })
+    res.json({
+        msg: 'You are signed up!',
+    })
+
+    res.status(403).send({
+        msg: 'User already exists!',
+    })
+    return
 })
 
 app.post('/signin', async (req, res) => {

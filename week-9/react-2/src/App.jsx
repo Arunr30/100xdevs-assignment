58d8cfc4731bbd7e8f26c86c8f6881db02e9652b
Arunvasuacchu <arunvasur@gmail.com>
2024-10-15 17:55:09 +0530

    learnt about useEffect

--- week-9/react-2/src/App.jsx
+++ week-9/react-2/src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 
+
 function App() {
-  const [count, setCount] = useState(0)
-  
-    setInterval(() => {
-      setCount(count + 1)
-    }, 1000)
     
+  const [count, setCount] = useState(0)
+    useEffect(() => {
+      setInterval(() => {
+         setCount(function(count) {
+          return count + 1
+         }) 
+      }, 1000)
+    },[])
   return (
     <>
     <h2>{count}</h2>
-
     </>
   )
 }

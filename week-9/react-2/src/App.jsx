b727b20cc3761b9cd45efe2248de4cd8c78dfd80
Arunvasuacchu <arunvasur@gmail.com>
2024-10-18 11:04:09 +0530

    learnt about mounting, unmounting, conditional rendering

--- week-9/react-2/src/App.jsx
+++ week-9/react-2/src/App.jsx
@@ -2,25 +2,44 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
-
-
 function App() {
-    
-  const [count, setCount] = useState(0)
-    useEffect(() => {
-      setInterval(() => {
-         setCount(function(count) {
-          return count + 1
-         }) 
-      }, 1000)
-    },[])
+    const[visible, setVisible] = useState(true)
+    useEffect(function() {
+        setInterval(() => {
+            setVisible(c => !c)
+        }, 5000)
+    }, [])
+  
   return (
+    // conditional rendering
     <>
-    <h2>{count}</h2>
+    {visible && <Counter />}
     </>
   )
 }
 
+function Counter() {
+    const[count, setCount] = useState(0)
+    useEffect(()=> {
+        // mountion, side effects!
+        let clock = setInterval(() => {
+            console.log("mounted!")
+            setCount(count => count + 1)
+        }, 1000)
+
+        // unmounting/ cleanup!
+        return function() {
+            console.log("unmounted!");
+            clearInterval(clock)
+        }
+    }, [])
+  
+    return(
+        <>
+        <h1> count is  {count}</h1>     
+        </>
+    )
+}
 
 
 export default App

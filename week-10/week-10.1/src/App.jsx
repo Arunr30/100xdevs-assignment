58c1f0a65f9ae07bea18273e62bb0131fbfc0c5d
Arunvasuacchu <arunvasur@gmail.com>
2024-10-25 11:18:53 +0530

    learnt about use ref usecases and build a simple stop watch

--- week-10/week-10.1/src/App.jsx
+++ week-10/week-10.1/src/App.jsx
@@ -1,33 +1,26 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { useRef, useState } from 'react'
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom'
 
 function App() {
+  const [count, setCount] = useState(0)
+  const timer = useRef()
+  function startCount() {
+    let value = setInterval(() => {
+      setCount((c) => c + 1)
+    }, 1000)
+    timer.current = value
+  }
+
+  function stopCount() {
+    clearInterval(timer.current)
+  }
   return (
     <div>
-      allen
-      <BrowserRouter>
-        <Link to={'/neet/class-11'}>Class 11</Link> <br />
-        <Link to={'/neet/class-12'}>Class 12</Link>
-        <br />
-        <Link to={'/'}>Allen</Link>
-        <Routes>
-          <Route path="/neet/class-11" element={<Class11 />} />
-          <Route path="/neet/class-12" element={<Class12 />} />
-          <Route path="/" element={<Index />} />
-        </Routes>
-      </BrowserRouter>
+      {count}
+      <button onClick={startCount}>start</button>
+      <button onClick={stopCount}>stop</button>
     </div>
   )
 }
 
-function Class11() {
-  return <div>hello from class 11</div>
-}
-
-function Class12() {
-  return <div>hello from class 12</div>
-}
-
-function Index() {
-  return <div>home page</div>
-}
 export default App

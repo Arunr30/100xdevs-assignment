41005bdf25fefb7eb0d728695396b8881469116e
Arunvasuacchu <arunvasur@gmail.com>
2024-10-25 13:12:43 +0530

    learnt about lightbulb functionality

--- week-10/state-exp/src/App.jsx
+++ week-10/state-exp/src/App.jsx
@@ -1,5 +1,36 @@
+import { useState } from 'react'
+
 function App() {
-    return <>hi there</>
+  return (
+    <>
+      <LightBulb />
+    </>
+  )
+}
+
+const LightBulb = () => {
+  const [bulbOn, setBulbOn] = useState(true)
+  return (
+    <div>
+      <BulbState bulbOn={bulbOn} />
+      <ToggleState bulbOn={bulbOn} setBulbOn={setBulbOn} />
+    </div>
+  )
+}
+
+const BulbState = ({ bulbOn }) => {
+  return <div>{bulbOn ? 'bulb on' : 'bulb off'}</div>
+}
+
+const ToggleState = ({ setBulbOn }) => {
+  function toggle() {
+    setBulbOn((currentState) => !currentState)
+  }
+  return (
+    <div>
+      <button onClick={toggle}>Toggle</button>
+    </div>
+  )
 }
 
 export default App

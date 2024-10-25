820657ba09a7eee8ac15ae4c4193dcddf5456344
Arunvasuacchu <arunvasur@gmail.com>
2024-10-25 12:52:23 +0530

    started week-10.2

--- week-10/week-10.1/src/App.jsx
+++ week-10/week-10.1/src/App.jsx
@@ -2,25 +2,25 @@ import { useRef, useState } from 'react'
 import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-  let timer = useRef()
-  function startCount() {
-    let value = setInterval(() => {
-      setCount((c) => c + 1)
-    }, 1000)
-    timer.current = value
-  }
+    const [count, setCount] = useState(0)
+    let timer = useRef()
+    function startCount() {
+        let value = setInterval(() => {
+            setCount((c) => c + 1)
+        }, 1000)
+        timer.current = value
+    }
 
-  function stopCount() {
-    clearInterval(timer.current)
-  }
-  return (
-    <div>
-      {count}
-      <button onClick={startCount}>start</button>
-      <button onClick={stopCount}>stop</button>
-    </div>
-  )
+    function stopCount() {
+        clearInterval(timer.current)
+    }
+    return (
+        <div>
+            {count}
+            <button onClick={startCount}>start</button>
+            <button onClick={stopCount}>stop</button>
+        </div>
+    )
 }
 
 export default App

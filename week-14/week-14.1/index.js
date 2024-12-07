a3e9cb7648c8e12e04d66b85d34283e54d8070c1
Arunvasuacchu <arunvasur@gmail.com>
2024-12-07 18:04:53 +0530

    typescript

---
+++ week-14/week-14.1/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function sum(a, b) {
+    return a + b;
+}
+let a = sum(1, 3);
+console.log(a);
---
+++ week-14/week-14.1/index.ts
@@ -0,0 +1,6 @@
+function sum(a:number, b: number) {
+  return a + b
+}
+
+let a = sum(1, 3)
+console.log(a)

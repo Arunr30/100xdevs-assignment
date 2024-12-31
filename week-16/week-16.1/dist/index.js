6d3891531a3daa168f2c8975521102ae75552146
Arunvasuacchu <arunvasur@gmail.com>
2024-12-31 16:21:03 +0530

    bootstraped a simple websocket server

--- week-16/week-16.1/dist/index.js
+++ week-16/week-16.1/dist/index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('hey');
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws"); // importing wss from ws.,
+const wss = new ws_1.WebSocketServer({ port: 5001 }); // defining a port
+// handling the event
+wss.on("connection", function (socket) {
+    console.log("user connected" + new Date());
+    setInterval(() => {
+        socket.send("hello");
+    }, 5000);
+    socket.on("message", (e) => {
+        console.log("reciever" + e.toString());
+    });
+});
--- week-16/week-16.1/src/index.ts
+++ week-16/week-16.1/src/index.ts
@@ -1,1 +1,21 @@
-console.log('hey')
+import { WebSocketServer } from "ws"; // importing wss from ws.,
+
+const wss = new WebSocketServer({port: 5001}) // defining a port
+
+
+// handling the event
+wss.on("connection", function(socket) {
+  console.log("user connected" + new Date());
+  setInterval(() => {
+    socket.send("hello")
+  },5000)
+
+  socket.on("message", (e) => {
+    console.log("reciever" + e.toString())
+  })
+})
+
+
+
+
+
